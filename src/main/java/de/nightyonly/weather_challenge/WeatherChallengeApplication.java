ff18b1eee5196b66e0399542df757f7a59ad2870
Nightyonlyy <business.nightyonly@pm.me>
2024-07-14 17:52:04 +0200

    Initial commit

---
+++ src/main/java/de/nightyonly/weather_challenge/WeatherChallengeApplication.java
@@ -0,0 +1,13 @@
+package de.nightyonly.weather_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherChallengeApplication.class, args);
+	}
+
+}
